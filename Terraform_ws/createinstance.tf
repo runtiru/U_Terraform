06d173348c5208596c3b5f58ec16ad87b7fa12f9
Ramgadiyasplk <tirumalagadiya@gmail.com>
2024-08-22 20:28:32 +0530

    delete provider.tf and add count

--- Terraform_ws/createinstance.tf
+++ Terraform_ws/createinstance.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "firstinstance" {
+resource "aws_instance" "myfirst" {
+  count = 2
   ami           = "ami-0b0ea68c435eb488d"
   instance_type = "t2.micro"
 }
--- Terraform_ws/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "AKIA3ISBV5LW3U5J3JNJ"
-  secret_key = "SECRET_KEY"
-  region     = "us-east-1"
-}
